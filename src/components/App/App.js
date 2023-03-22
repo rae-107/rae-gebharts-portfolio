0a2d6a1e80d0b05478879d936eb66a02944e644d
Rae Gebhart <raegebhart@gmail.com>
2023-03-21 20:06:29 -0600

    Add Home component to element property

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Home from '../Home/Home'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return(
     <main className='app'>
       <Routes>
-        <Route path='/' ></Route>
+        <Route path='/' element={<Home/>}></Route>
         <Route path='/projects' ></Route>
         <Route path='/about' ></Route>
         <Route path='/contact' ></Route>
