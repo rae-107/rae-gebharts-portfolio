e5699fb824eb773bf0b931c3eda0bff185563bde
Rae Gebhart <raegebhart@gmail.com>
2023-04-07 22:00:05 -0600

    Add career summary and title sections

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,31 @@ import { NavBar } from '../NavBar/NavBar'
 
 const Home = () => {
   return (
+    <div>
     <div className='home'>
-      <NavBar />
-      <h1>Hello</h1>
+      <div className='nav-container' >
+        <div className='nav-style-box'></div>
+        <NavBar />
+        <img className='home-icon' src='/assets/home-stencil.png'/>
+      </div>
+      <div className='intro-container'>
+        <div className='image-container' >
+          <img className='image' src='/assets/square-profile.jpg' />
+          <h1>Rae Gebhart</h1>
+        </div>
+      </div>
+      <div className='title-container' >
+        <div className='title-border-container' >
+          <h2 className='professional-title' >~ Professional Title ~</h2>
+          <h2 className='software-developer'>Software Developer</h2>
+        </div>
+      </div>
+      <div className='career-summary-container'>
+        <div className='career-border-container' >
+          <h2>Career Summary</h2>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
