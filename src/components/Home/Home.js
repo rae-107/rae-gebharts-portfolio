fc7df45dcac9283b649010469c1a1ad7e3e2ae01
Rae Gebhart <raegebhart@gmail.com>
2023-04-07 18:24:05 -0600

    Import and implement NavBar into Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import './Home.css'
+import { NavBar } from '../NavBar/NavBar'
 
 const Home = () => {
   return (
     <div className='home'>
+      <NavBar />
       <h1>Hello</h1>
     </div>
   )
