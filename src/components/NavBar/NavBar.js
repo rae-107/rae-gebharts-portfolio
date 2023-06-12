99f126b38c3df80fbb236e06daff01b8918d40d5
Rae Gebhart <raegebhart@gmail.com>
2023-06-12 12:08:24 -0600

    Add home to nav bar and remove home image and other unused images

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,19 +4,12 @@ import "./NavBar.css";
 export const NavBar = () => {
   return (
     <div className="nav-container">
-      <div className="nav-style-box"></div>
       <nav>
+        <NavLink to={"/"} >Home</NavLink>
         <NavLink to={"/projects"}>Projects</NavLink>
         <NavLink to={"/about"}>About</NavLink>
         <NavLink to={"/contact"}>Contact</NavLink>
       </nav>
-      <Link to='/'>
-      <img
-        className="home-icon"
-        alt="Home Button"
-        src="/assets/home-stencil.png"
-      />
-      </Link>
     </div>
   );
 };
